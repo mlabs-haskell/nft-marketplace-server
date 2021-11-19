1027800b135a53df25bb02c50c2a6b5347dc2484
Kirill Boltaev <aske@fmap.me>
2021-11-19 13:36:25 +0300

    Throw 401 on decodeUtf8 failures during auth

--- src/Api/Auth.hs
+++ src/Api/Auth.hs
@@ -1,7 +1,7 @@
 module Api.Auth (authHandler) where
 
 import Control.Monad.IO.Class (liftIO)
-import Data.Text.Encoding (decodeUtf8)
+import Data.Text.Encoding (decodeUtf8')
 import Network.Wai (Request, requestHeaders)
 import Servant (err401, throwError)
 import Servant.Server.Experimental.Auth (AuthHandler, mkAuthHandler)
@@ -15,18 +15,18 @@ import Schema
 authHandler :: Env -> AuthHandler Request ()
 authHandler env = mkAuthHandler handler
   where
-    throwUnathorized = throwError err401
+    throwUnauthorized = throwError err401
 
     handler req = do
         let Env{..} = env
-        authHeader <- maybe throwUnathorized pure . lookup "Authorization" $ requestHeaders req
+        authHeader <- maybe throwUnauthorized pure . lookup "Authorization" $ requestHeaders req
         -- TODO: change to `decodeUft8'`
-        let authHeaderText = decodeUtf8 authHeader
+        authHeaderText <- either (const throwUnauthorized) pure $ decodeUtf8' authHeader
         adminToken <- liftIO $
             flip P.runSqlPersistMPool dbConnPool $ do
                 selectOne $ do
                     token <- from $ table @AdminToken
                     where_ (token ^. AdminTokenToken ==. val authHeaderText)
         case adminToken of
-            Nothing -> throwUnathorized
+            Nothing -> throwUnauthorized
             Just _ -> pure ()
