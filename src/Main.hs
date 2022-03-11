57c95ebbaed8d922e8847a90fdf80770e3c1971f
Mikhail Lazarev <mikhail@mlabs.city>
2022-03-11 17:15:19 +0300

    fix migration that doesn't happen

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,35 @@
 module Main (main) where
 
+-- import Control.Monad.Logger (runStderrLoggingT)
+
+-- import Database.Persist.Sql.Migration (addMigration)
+
+import Api (Routes, marketplaceApi)
+import Api.Auth (authHandler)
+import Api.Handler (handlers)
+import App (App (..))
 import Control.Monad.Catch (try)
 import Control.Monad.Except (ExceptT (..))
 import Control.Monad.IO.Class (liftIO)
-
--- import Control.Monad.Logger (runStderrLoggingT)
 import Control.Monad.Logger (runNoLoggingT)
 import Control.Monad.Reader (runReaderT)
 import Data.ByteString.Char8 qualified as BSC
 import Data.Text qualified as Text
-import Database.Persist.Postgresql (runMigration, runSqlPersistMPool, withPostgresqlPool)
-
--- import Database.Persist.Sql.Migration (addMigration)
-
+import Database.Persist.Postgresql
+  ( runMigration,
+    runSqlPersistMPool,
+    withPostgresqlPool,
+  )
+import Env (Env (..))
 import Network.HTTP.Client qualified as HttpClient
 import Network.Wai (Request)
 import Network.Wai.Handler.Warp qualified as W
 import Network.Wai.Logger (withStdoutLogger)
 import Network.Wai.Middleware.Cors (simpleCors)
 import Network.Wai.Parse (defaultParseRequestBodyOptions, setMaxRequestFileSize)
+import Options (Options (..))
+import Options qualified
+import Schema (migrateAll)
 import Servant (Application, Context (..), Handler (..), Proxy (..), ServerT, hoistServerWithContext, serveWithContext)
 import Servant.API.Generic (ToServantApi)
 import Servant.Client (mkClientEnv, parseBaseUrl)
@@ -27,25 +38,16 @@ import Servant.Server.Experimental.Auth (AuthHandler)
 import Servant.Server.Generic (genericServerT)
 import System.Directory (createDirectoryIfMissing)
 
-import Api (Routes, marketplaceApi)
-import Api.Auth (authHandler)
-import Api.Handler (handlers)
-import App (App (..))
-import Env (Env (..))
-import Options (Options (..))
-import Options qualified
-import Schema (migrateAll)
-
 appService :: Env -> Application
 appService env = serveWithContext marketplaceApi ctx appServer
   where
     ctx = authHandler env :. multipartOpts :. EmptyContext
 
     multipartOpts =
-        (defaultMultipartOptions (Proxy :: Proxy Tmp))
-            { -- Disallow files > 2MiB
-              generalOptions = setMaxRequestFileSize (2 * 1024 * 1024) defaultParseRequestBodyOptions
-            }
+      (defaultMultipartOptions (Proxy :: Proxy Tmp))
+        { -- Disallow files > 2MiB
+          generalOptions = setMaxRequestFileSize (2 * 1024 * 1024) defaultParseRequestBodyOptions
+        }
 
     hoistApp :: App a -> Handler a
     hoistApp = Handler . ExceptT . try . flip runReaderT env . unApp
@@ -61,27 +63,27 @@ appService env = serveWithContext marketplaceApi ctx appServer
 
 main :: IO ()
 main = do
-    Options{..} <- Options.parseOptions
-    let imageFolderText = Text.pack imageFolder
-    let connStr = BSC.pack dbConnectionString
+  Options {..} <- Options.parseOptions
+  let imageFolderText = Text.pack imageFolder
+  let connStr = BSC.pack dbConnectionString
 
-    createDirectoryIfMissing False imageFolder
+  createDirectoryIfMissing False imageFolder
 
-    ipfsNodeUrl <- parseBaseUrl ipfsNodeAddress
-    manager <- HttpClient.newManager HttpClient.defaultManagerSettings
-    let clientEnv = mkClientEnv manager ipfsNodeUrl
+  ipfsNodeUrl <- parseBaseUrl ipfsNodeAddress
+  manager <- HttpClient.newManager HttpClient.defaultManagerSettings
+  let clientEnv = mkClientEnv manager ipfsNodeUrl
 
-    runNoLoggingT $
-        withPostgresqlPool connStr 10 $ \pool ->
-            liftIO $
-                flip runSqlPersistMPool pool $ do
-                    runMigration $ do
-                        migrateAll
-                    -- addMigration True "CREATE INDEX CONCURRENTLY IF NOT EXISTS image_created_at_index ON image (created_at)"
-                    -- addMigration True "CREATE INDEX CONCURRENTLY IF NOT EXISTS artist_created_at_index ON artist (created_at)"
-                    let env = Env pool imageFolderText clientEnv
+  runNoLoggingT $
+    withPostgresqlPool connStr 10 $ \pool -> do
+      liftIO $ -- run migrations
+        flip runSqlPersistMPool pool $ do
+          runMigration $ do
+            migrateAll
+      -- addMigration True "CREATE INDEX CONCURRENTLY IF NOT EXISTS image_created_at_index ON image (created_at)"
+      -- addMigration True "CREATE INDEX CONCURRENTLY IF NOT EXISTS artist_created_at_index ON artist (created_at)"
 
-                    liftIO $
-                        withStdoutLogger $ \logger -> do
-                            let warpSettings = W.setPort serverPort $ W.setLogger logger W.defaultSettings
-                            W.runSettings warpSettings $ simpleCors (appService env)
+      liftIO $ -- start server
+        withStdoutLogger $ \logger -> do
+          let env = Env pool imageFolderText clientEnv
+              warpSettings = W.setPort serverPort $ W.setLogger logger W.defaultSettings
+          W.runSettings warpSettings $ simpleCors (appService env)
