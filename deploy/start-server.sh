45bbb739d931d99fe3b784fd6d6dd96916c844f6
Mikhail Lazarev <mikhail@mlabs.city>
2022-03-14 17:46:40 +0300

    some fixes

--- deploy/start-server.sh
+++ deploy/start-server.sh
@@ -8,12 +8,13 @@ set -euo pipefail
 # export PG_SBUG_USER=seadebug
 # export PG_SBUG_PASS=654321
 
-# E.g.: ./start-server.sh --image-folder /home/ubuntu/seabug/images   
+# E.g.: ./start-server.sh --image-folder /home/ubuntu/seabug/images
 cd ..
-nix build -f release.nix
+. ~/.nix-profile/etc/profile.d/nix.sh
+nix build -f release.nix --extra-experimental-features nix-command
 
 ./result/bin/nft-marketplace-server \
   -p 8008 \
   --db-connection "postgresql://$PG_SBUG_USER:$PG_SBUG_PASS@localhost:5432/$PG_SBUG_USER" \
   --ipfs-node "localhost:5001"\
-  "$@" # for specifying images directory
+  "$@" & # for specifying images directory
