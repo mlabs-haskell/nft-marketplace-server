bc53802ad2bbb55e4f3d8042dd7d52753e1d7f9d
t4ccer <t4ccer@gmail.com>
2022-04-05 16:37:50 -0600

    Add headers to cors

--- src/Main.hs
+++ src/Main.hs
@@ -26,7 +26,7 @@ import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Network.Wai (Request)
 import Network.Wai.Handler.Warp qualified as W
 import Network.Wai.Logger (withStdoutLogger)
-import Network.Wai.Middleware.Cors (simpleCors)
+import Network.Wai.Middleware.Cors (cors, corsExposedHeaders, corsRequestHeaders, simpleCorsResourcePolicy)
 import Network.Wai.Parse (defaultParseRequestBodyOptions, setMaxRequestFileSize)
 import Options (NftDbOptions (..), Options (..))
 import Options qualified
@@ -94,4 +94,19 @@ main = do
                 withStdoutLogger $ \logger -> do
                     let env = Env pool imageFolderText clientEnv
                         warpSettings = W.setPort serverPort $ W.setLogger logger W.defaultSettings
-                    W.runSettings warpSettings $ simpleCors (appService env)
+                        customCorsPolicy =
+                            simpleCorsResourcePolicy
+                                { corsRequestHeaders =
+                                    [ "Authorization"
+                                    , "Range"
+                                    ]
+                                , corsExposedHeaders =
+                                    Just
+                                        [ "Total-Count"
+                                        , "Accept-Ranges"
+                                        , "Content-Range"
+                                        , "Next-Range"
+                                        ]
+                                }
+                        customCors = cors (const $ Just customCorsPolicy)
+                    W.runSettings warpSettings $ customCors (appService env)
