47156150be1e708d61ba40b93a56689ee161e27e
Samuel Williams <williamssamuel1999@gmail.com>
2022-07-28 10:16:09 +0100

    Formatting

--- src/NftStorage.hs
+++ src/NftStorage.hs
@@ -1,8 +1,7 @@
-module NftStorage
-  ( nftStorageAdd,
+module NftStorage (
+    nftStorageAdd,
     CID (..),
-  )
-where
+) where
 
 import App (App)
 import Control.Monad.IO.Class (liftIO)
@@ -21,47 +20,47 @@ import Servant.Client (ClientEnv, client, runClientM)
 data PNG deriving stock (Typeable)
 
 instance Accept PNG where
-  contentType _ = "image" // "png"
+    contentType _ = "image" // "png"
 
 instance MimeRender PNG BS.ByteString where
-  mimeRender _ = BSL.fromStrict
+    mimeRender _ = BSL.fromStrict
 
 data JPEG deriving stock (Typeable)
 
 instance Accept JPEG where
-  contentType _ = "image" // "jpeg"
+    contentType _ = "image" // "jpeg"
 
 instance MimeRender JPEG BS.ByteString where
-  mimeRender _ = BSL.fromStrict
+    mimeRender _ = BSL.fromStrict
 
 type NftStorageApi =
-  "upload"
-    :> ReqBody '[PNG, JPEG] BS.ByteString
-    :> Header "Authorization" String
-    :> Post '[JSON] Value
+    "upload"
+        :> ReqBody '[PNG, JPEG] BS.ByteString
+        :> Header "Authorization" String
+        :> Post '[JSON] Value
 
 nftStorageApi :: Proxy NftStorageApi
 nftStorageApi = Proxy
 
 nftStorageAdd :: ClientEnv -> String -> BS.ByteString -> App (Either String CID)
 nftStorageAdd nftStorageClientEnv apiKey fileContents = do
-  result <- liftIO $ runClientM query nftStorageClientEnv
-  let result' = do
-        body <- case result of
-          Left e -> Left ("Error making an nft.storage client request: " <> show e)
-          Right (Object body) -> pure body
-          _ -> Left "Error making an nft.storage client request: wrong response format"
-        val <- case KeyMap.lookup "value" body of
-          Just (Object val) -> pure val
-          _ -> Left "Error making an ipfs client request: wrong response format"
-        case KeyMap.lookup "cid" val of
-          Just (String cid) -> pure cid
-          _ -> Left "Error making an ipfs client request: wrong response format"
-  case result' of
-    Left e -> do
-      liftIO $ putStrLn e
-      pure $ Left e
-    Right v -> pure $ bimap ("error while enconding CID: " ++) (CID . pack) (encodeBase32InBase36 (unpack v))
+    result <- liftIO $ runClientM query nftStorageClientEnv
+    let result' = do
+            body <- case result of
+                Left e -> Left ("Error making an nft.storage client request: " <> show e)
+                Right (Object body) -> pure body
+                _ -> Left "Error making an nft.storage client request: wrong response format"
+            val <- case KeyMap.lookup "value" body of
+                Just (Object val) -> pure val
+                _ -> Left "Error making an ipfs client request: wrong response format"
+            case KeyMap.lookup "cid" val of
+                Just (String cid) -> pure cid
+                _ -> Left "Error making an ipfs client request: wrong response format"
+    case result' of
+        Left e -> do
+            liftIO $ putStrLn e
+            pure $ Left e
+        Right v -> pure $ bimap ("error while enconding CID: " ++) (CID . pack) (encodeBase32InBase36 (unpack v))
   where
     nftStorageClient = client nftStorageApi
     query = nftStorageClient fileContents (pure ("Bearer " <> apiKey))
